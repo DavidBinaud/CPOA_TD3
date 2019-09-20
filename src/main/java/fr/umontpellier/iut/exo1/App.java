353713054cf64fe3945800b736b9195abc55cd5a
David Binaud <david.binaud@gmail.com>
2019-09-20 14:56:15 +0200

    Ex1 Q3 Test Utilisateur dans App

--- src/main/java/fr/umontpellier/iut/exo1/App.java
+++ src/main/java/fr/umontpellier/iut/exo1/App.java
@@ -2,6 +2,9 @@ package fr.umontpellier.iut.exo1;
 
 public class App {
     public static void main(String[] args) {
-        throw new RuntimeException("méthode non-implémentée");
+
+        Utilisateur utilisateur = new Utilisateur();
+        utilisateur.effectuerOperation("+",5);
+        System.out.println(utilisateur);
     }
 }
