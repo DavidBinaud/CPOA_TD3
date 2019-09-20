195d6819f8fdb4f15a226d3eea9f4264bb05bb22
David Binaud <david.binaud@gmail.com>
2019-09-20 16:02:27 +0200

    Ex1 Q8 Utilisateur methode refaireoperation()

--- src/main/java/fr/umontpellier/iut/exo1/Utilisateur.java
+++ src/main/java/fr/umontpellier/iut/exo1/Utilisateur.java
@@ -7,21 +7,35 @@ public class Utilisateur {
 
     private Calculette macalculette = new Calculette();
     private ArrayDeque<Command> historique= new ArrayDeque<>();
+    private ArrayDeque<Command> historiqueannulation = new ArrayDeque<>();
 
 
     public void effectuerOperation(String operation, double operande){
         Command cmd = new Command(operation,operande,macalculette);
         cmd.executer();
         historique.push(cmd);
+        historiqueannulation.clear();
     }
 
     public void annulerOperation(int nboperations){
 
         while (!historique.isEmpty() && nboperations > 0){
-            historique.pop().annuler();
+            Command c = historique.pop();
+            c.annuler();
+            historiqueannulation.push(c);
             nboperations--;
         }
 
+    }
+
+    public void refaireOperation(int nboperations){
+
+        while (!historiqueannulation.isEmpty() && nboperations > 0){
+            Command c = historiqueannulation.pop();
+            c.annuler();
+            historique.push(c);
+            nboperations--;
+        }
     }
 
     @Override
