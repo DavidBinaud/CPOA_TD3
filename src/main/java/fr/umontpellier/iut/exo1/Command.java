e44125273ec90170b9a935c72d733d95deab0eb0
David Binaud <david.binaud@gmail.com>
2019-09-20 15:18:32 +0200

    Ex1 Q5 Creation classe Command

---
+++ src/main/java/fr/umontpellier/iut/exo1/Command.java
@@ -0,0 +1,42 @@
+package fr.umontpellier.iut.exo1;
+
+public class Command {
+
+    private String operateur;
+
+    private double operande;
+
+    private Calculette calculette;
+
+    public Command(String operateur, double operande, Calculette calculette) {
+        this.operateur = operateur;
+        this.operande = operande;
+        this.calculette = calculette;
+    }
+
+    void executer(){
+        calculette.calculer(operateur,operande);
+    }
+
+    public void annuler(){
+        calculette.calculer(inverse(operateur),operande);
+    }
+
+    public String inverse(String operateur){
+        switch (operateur) {
+            case "+":
+                return "-";
+
+            case "-":
+                return "+";
+
+            case "*":
+                return "/";
+
+            case "/":
+                return "*";
+            default: return "";
+        }
+    }
+}
+
