1bdd76cceb4cf1b72ee3e003f80c24f7567aa3e4
David Binaud <david.binaud@gmail.com>
2019-09-20 15:41:06 +0200

    Ex1 Q7 Utilisation dans APP

--- src/main/java/fr/umontpellier/iut/exo1/App.java
+++ src/main/java/fr/umontpellier/iut/exo1/App.java
@@ -4,7 +4,17 @@ public class App {
     public static void main(String[] args) {
 
         Utilisateur utilisateur = new Utilisateur();
-        utilisateur.effectuerOperation("+",5);
-        System.out.println(utilisateur);
+
+
+        for (int j = 0; j <= 100; j++) {
+            utilisateur.effectuerOperation("+",j);
+            System.out.println(utilisateur.toString());
+        }
+
+
+        for (int j = 0; j < 100; j++) {
+            utilisateur.annulerOperation(1);
+            System.out.println(utilisateur.toString());
+        }
     }
 }
