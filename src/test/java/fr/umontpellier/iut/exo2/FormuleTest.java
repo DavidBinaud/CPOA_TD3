3107c2a1bbb15c31ada6659cfc279e83040ce6aa
David Binaud <david.binaud@gmail.com>
2019-09-27 15:31:40 +0200

    Ex2 Q1 FormuleTest ajout de Tests pour getValeur

--- src/test/java/fr/umontpellier/iut/exo2/FormuleTest.java
+++ src/test/java/fr/umontpellier/iut/exo2/FormuleTest.java
@@ -35,4 +35,35 @@ class FormuleTest {
         assertEquals(f.toString(), "{[21, -, 9, +, 5, *, 20, /, (, 947, -, 6, -, 891, ), +, 18]}");
     }
 
+
+    @Test
+    public void test_value_2_times_3(){
+        Formule f = new Formule("2*3");
+        assertEquals(f.getValeur(),6.0);
+    }
+
+    @Test
+    public void test_value_1() {
+        Formule f = new Formule("1");
+        assertEquals(f.getValeur(), 1.0);
+    }
+
+    @Test
+    public void test_value_5_minus_3_plus_2() {
+        Formule f = new Formule("5 - 3 + 2");
+        assertEquals(f.getValeur(), 4.0);
+    }
+
+    @Test
+    public void test_value_long_formula() {
+        Formule f = new Formule("100*3.2-(100+50*(2-1))");
+        assertEquals(f.getValeur(), 170.0);
+    }
+
+    @Test
+    public void test_value_long_formula_bis() {
+        Formule f = new Formule("21-9+5*20/(947  - 6 - 891)+18");
+        assertEquals(f.getValeur(), 32.0);
+    }
+
 }
