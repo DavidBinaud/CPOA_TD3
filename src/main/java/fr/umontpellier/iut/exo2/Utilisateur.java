4b9917fd18d45d8726f195b835132a888dc28521
David Binaud <david.binaud@gmail.com>
2019-10-02 08:35:59 +0200

    Ex2 Q5 Utilisateur Refactorisation Avec des Commandes pour les historique et les méthodes Cassage de la dépendances avec calculette

--- src/main/java/fr/umontpellier/iut/exo2/Utilisateur.java
+++ src/main/java/fr/umontpellier/iut/exo2/Utilisateur.java
@@ -4,13 +4,11 @@ import java.util.ArrayDeque;
 
 public class Utilisateur {
 
-    private Calculette macalculette = new Calculette();
-    private ArrayDeque<CommandeNombre> historique= new ArrayDeque<>();
-    private ArrayDeque<CommandeNombre> historiqueannulation = new ArrayDeque<>();
+    private ArrayDeque<Commande> historique= new ArrayDeque<>();
+    private ArrayDeque<Commande> historiqueannulation = new ArrayDeque<>();
 
 
-    public void effectuerOperation(String operation, double operande){
-        CommandeNombre cmd = new CommandeNombre(operation,operande,macalculette);
+    public void effectuerOperation(Commande cmd){
         cmd.executer();
         historique.push(cmd);
         historiqueannulation.clear();
@@ -19,7 +17,7 @@ public class Utilisateur {
     public void annulerOperation(int nboperations){
 
         while (!historique.isEmpty() && nboperations > 0){
-            CommandeNombre c = historique.pop();
+            Commande c = historique.pop();
             c.annuler();
             historiqueannulation.push(c);
             nboperations--;
@@ -30,15 +28,11 @@ public class Utilisateur {
     public void refaireOperation(int nboperations){
 
         while (!historiqueannulation.isEmpty() && nboperations > 0){
-            CommandeNombre c = historiqueannulation.pop();
+            Commande c = historiqueannulation.pop();
             c.executer();
             historique.push(c);
             nboperations--;
         }
     }
 
-    @Override
-    public String toString() {
-        return macalculette.toString();
-    }
 }
