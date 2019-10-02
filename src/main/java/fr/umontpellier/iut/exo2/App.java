06aecab132faf466b622a2603bc60138e9615c89
David Binaud <david.binaud@gmail.com>
2019-10-02 08:25:06 +0200

    Ex2 Q4 App réalisation des étapes Validée

--- src/main/java/fr/umontpellier/iut/exo2/App.java
+++ src/main/java/fr/umontpellier/iut/exo2/App.java
@@ -3,13 +3,46 @@ package fr.umontpellier.iut.exo2;
 public class App {
     public static void main(String[] args) {
 
-       Formule formule = new Formule("(200+50)*5+9/3");
-        System.out.println(formule.toString());
-        System.out.println(formule.getValeur());
+        Utilisateur u = new Utilisateur();
 
 
-        Formule formulelongue = new Formule("21-9+5*20/(947  - 6 - 891)+18");
-        System.out.println(formulelongue.toString());
-        System.out.println(formulelongue.getValeur());
+        u.effectuerOperation("+",5);
+
+        u.effectuerOperation("*",3);
+
+        Formule formule1 = new Formule("(100*3.2-(100+50*(2-1)))");
+        u.effectuerOperation("+",formule1.getValeur());
+
+
+        u.effectuerOperation("+",13);
+        Formule formule2 = new Formule("(21-9+5*20/(947 - 6 - 891)+18)");
+        u.effectuerOperation("-",formule2.getValeur());
+
+        //devrait afficher 166
+        System.out.println(u.toString());
+
+        u.annulerOperation(3);
+
+        //devrait afficher 15
+        System.out.println(u.toString());
+
+        u.refaireOperation(1);
+
+
+        //devrait afficher 185
+        System.out.println(u.toString());
+
+        u.effectuerOperation("+",1);
+
+
+        //devrait afficher 186
+        System.out.println(u.toString());
+
+        // ne refait aucune operation
+        u.refaireOperation(1);
+
+
+        //devrait afficher 186
+        System.out.println(u.toString());
     }
 }
