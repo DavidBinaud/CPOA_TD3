8a6955825ce9ffb0cf93fd4436a45a2707476316
David Binaud <david.binaud@gmail.com>
2019-10-02 08:36:44 +0200

    Ex2 Q5 Changement de l'implementation pour coller à la Q4 avec des Commandes donné à l'utilisateur

--- src/main/java/fr/umontpellier/iut/exo2/App.java
+++ src/main/java/fr/umontpellier/iut/exo2/App.java
@@ -4,45 +4,44 @@ public class App {
     public static void main(String[] args) {
 
         Utilisateur u = new Utilisateur();
+        Calculette calculette = new Calculette();
+
+        u.effectuerOperation(new CommandeNombre("+", 5,calculette));
+
+        u.effectuerOperation(new CommandeNombre("*",3,calculette));
+
+        u.effectuerOperation(new CommandeFormule("+","(100*3.2-(100+50*(2-1)))",calculette));
 
 
-        u.effectuerOperation("+",5);
-
-        u.effectuerOperation("*",3);
-
-        Formule formule1 = new Formule("(100*3.2-(100+50*(2-1)))");
-        u.effectuerOperation("+",formule1.getValeur());
-
-
-        u.effectuerOperation("+",13);
+        u.effectuerOperation(new CommandeNombre("+",13, calculette));
         Formule formule2 = new Formule("(21-9+5*20/(947 - 6 - 891)+18)");
-        u.effectuerOperation("-",formule2.getValeur());
+        u.effectuerOperation(new CommandeFormule("-","(21-9+5*20/(947 - 6 - 891)+18)",calculette));
 
         //devrait afficher 166
-        System.out.println(u.toString());
+        System.out.println(calculette.toString());
 
         u.annulerOperation(3);
 
         //devrait afficher 15
-        System.out.println(u.toString());
+        System.out.println(calculette.toString());
 
         u.refaireOperation(1);
 
 
         //devrait afficher 185
-        System.out.println(u.toString());
+        System.out.println(calculette.toString());
 
-        u.effectuerOperation("+",1);
+        u.effectuerOperation(new CommandeNombre("+",1,calculette));
 
 
         //devrait afficher 186
-        System.out.println(u.toString());
+        System.out.println(calculette.toString());
 
         // ne refait aucune operation
         u.refaireOperation(1);
 
 
         //devrait afficher 186
-        System.out.println(u.toString());
+        System.out.println(calculette.toString());
     }
 }
